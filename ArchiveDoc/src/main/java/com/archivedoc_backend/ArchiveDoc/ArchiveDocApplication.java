4dc285f4d6b4b49a02e602bdd5bd5a39bd630995
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-01-09 23:48:12 -0300

    Configurações

---
+++ ArchiveDoc/src/main/java/com/archivedoc_backend/ArchiveDoc/ArchiveDocApplication.java
@@ -0,0 +1,13 @@
+package com.archivedoc_backend.ArchiveDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiveDocApplication.class, args);
+	}
+
+}
